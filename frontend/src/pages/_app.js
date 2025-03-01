d41bf9e3d5e893b4ff375b7b50eba381edfbaabe
evertonethan <ethan.emanuelmessias@gmail.com>
2025-02-28 22:33:48 -0300

    Frontend - src/pages/_app.js criado

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,17 @@
+import '../styles/globals.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <main style={{ flex: 1 }}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default MyApp;
