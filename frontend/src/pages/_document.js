41e59990e88bbc09303eb92c2034a5c00327b946
evertonethan <ethan.emanuelmessias@gmail.com>
2025-02-28 22:33:12 -0300

    Frontend - src/pages/_document.js criado

---
+++ frontend/src/pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="E-commerce simples com Stripe" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
