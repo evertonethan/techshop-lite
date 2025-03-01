7ca89e62c5fca26b40a6480d755815867188a02c
evertonethan <ethan.emanuelmessias@gmail.com>
2025-03-01 08:48:16 -0300

    update src/pages/_app.js (atualizado com CartProvider)

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,16 +1,19 @@
 import '../styles/globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { CartProvider } from '../context/CartContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Header />
-      <main style={{ flex: 1 }}>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <CartProvider>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <main style={{ flex: 1 }}>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </CartProvider>
   );
 }
 
